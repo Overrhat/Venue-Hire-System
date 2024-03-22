37766873d61bb47eeb035bca2ac2621436f558cf
Jeruh1105 <naruh061105@gmail.com>
2024-03-22 21:26:43 +1300

    Change in printVenues in VenueHireSystme class

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -1,19 +1,74 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+
 import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
 
 public class VenueHireSystem {
 
-  public VenueHireSystem() {}
+  private ArrayList venueList = new ArrayList<>();
+  private String venueName;
+  private String venueCode;
+  private int capacity;
+  private int hireFee;
+
+  public VenueHireSystem() {
+    // this.venueName = venueName;
+    // this.venueCode = venueCode;
+    // this.capacity = Integer.parseInt(capacityInput);
+    // this.hireFee = Integer.parseInt(hireFeeInput);
+    // MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
+  }
 
   public void printVenues() {
-    // TODO implement this method
+    // finding the size of the venue list to check how many venues are in the list
+    int venueNumber = venueList.size();
+
+    // using switch to print the correct way for each cases
+    switch (venueNumber) {
+      case 0:
+        MessageCli.NO_VENUES.printMessage();
+        break;
+      case 1:
+        MessageCli.NUMBER_VENUES.printMessage("is", "one", "");
+        break;
+      case 2:
+        MessageCli.NUMBER_VENUES.printMessage("are", "two", "s");
+        break;
+      case 3:
+        MessageCli.NUMBER_VENUES.printMessage("are", "one", "s");
+        break;
+      case 4:
+        MessageCli.NUMBER_VENUES.printMessage("are", "one", "s");
+        break;
+      case 5:
+        MessageCli.NUMBER_VENUES.printMessage("are", "one", "s");
+        break;
+      case 6:
+        MessageCli.NUMBER_VENUES.printMessage("are", "one", "s");
+        break;
+      case 7:
+        MessageCli.NUMBER_VENUES.printMessage("are", "one", "s");
+        break;
+      case 8:
+        MessageCli.NUMBER_VENUES.printMessage("are", "one", "s");
+        break;
+      case 9:
+        MessageCli.NUMBER_VENUES.printMessage("are", "one", "s");
+        break;
+      default:
+        MessageCli.NUMBER_VENUES.printMessage("are", "one", "s");
+        break;
+  
+        // Print all the venue list
+        
+    }
   }
 
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
-    // TODO implement this method
+        
   }
 
   public void setSystemDate(String dateInput) {
