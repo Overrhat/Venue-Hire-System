641f9503062644783bf9f614e8793818c2f32f2e
Jeruh1105 <naruh061105@gmail.com>
2024-04-07 02:26:34 +1300

    Added isOtherDatePast method to Dates class

--- src/main/java/nz/ac/auckland/se281/Dates.java
+++ src/main/java/nz/ac/auckland/se281/Dates.java
@@ -2,15 +2,21 @@ package nz.ac.auckland.se281;
 
 public class Dates {
 
+  private String fullDates;
   private String date;
   private String month;
   private String year;
 
   public Dates(String fullDates) {
+    this.fullDates = fullDates;
     String[] dateParts = fullDates.split("/");
     this.date = dateParts[0];
     this.month = dateParts[1];
     this.year = dateParts[2];
+  }
+
+  public String getFullDates() {
+    return this.fullDates;
   }
 
   public String getDate() {
@@ -23,5 +29,13 @@ public class Dates {
 
   public String getYear() {
     return this.year;
+  }
+
+  public Boolean isOtherDatePast(Dates other) {
+    // Initialize the other date is past
+    Boolean isPast = true;
+
+    // Return the result
+    return isPast;
   }
 }
