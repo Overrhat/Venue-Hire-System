ea880660b98c8b26cb6799ce5cb150454783230a
Jeruh1105 <naruh061105@gmail.com>
2024-04-07 02:12:02 +1300

    Added getter methods for the Dates class

--- src/main/java/nz/ac/auckland/se281/Dates.java
+++ src/main/java/nz/ac/auckland/se281/Dates.java
@@ -2,16 +2,26 @@ package nz.ac.auckland.se281;
 
 public class Dates {
 
-  private String fullDates;
   private String date;
   private String month;
   private String year;
 
   public Dates(String fullDates) {
-    this.fullDates = fullDates;
     String[] dateParts = fullDates.split("/");
     this.date = dateParts[0];
     this.month = dateParts[1];
     this.year = dateParts[2];
+  }
+
+  public String getDate() {
+    return this.date;
+  }
+
+  public String getMonth() {
+    return this.month;
+  }
+
+  public String getYear() {
+    return this.year;
   }
 }
