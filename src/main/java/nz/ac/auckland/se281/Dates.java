e77cd108c0653bdae94b8b6317dc65ea35c22c86
Jeruh1105 <naruh061105@gmail.com>
2024-04-07 21:38:30 +1200

    Changed numbers in Dates class to treat as integers

--- src/main/java/nz/ac/auckland/se281/Dates.java
+++ src/main/java/nz/ac/auckland/se281/Dates.java
@@ -3,37 +3,39 @@ package nz.ac.auckland.se281;
 public class Dates {
 
   private String fullDates;
-  private String date;
-  private String month;
-  private String year;
+  private int date;
+  private int month;
+  private int year;
 
   public Dates(String fullDates) {
     this.fullDates = fullDates;
     String[] dateParts = fullDates.split("/");
-    this.date = dateParts[0];
-    this.month = dateParts[1];
-    this.year = dateParts[2];
+    this.date = Integer.parseInt(dateParts[0]);
+    this.month = Integer.parseInt(dateParts[1]);
+    this.year = Integer.parseInt(dateParts[2]);
   }
 
   public String getFullDates() {
     return this.fullDates;
   }
 
-  public String getDate() {
+  public int getDate() {
     return this.date;
   }
 
-  public String getMonth() {
+  public int getMonth() {
     return this.month;
   }
 
-  public String getYear() {
+  public int getYear() {
     return this.year;
   }
 
   public Boolean isOtherDatePast(Dates other) {
     // Initialize the other date is past
     Boolean isPast = true;
+
+    // Check the year
 
     // Return the result
     return isPast;
