f7a6bc558743752f560ea9f74439cee5d09c3d34
Jeruh1105 <naruh061105@gmail.com>
2024-04-09 16:57:41 +1200

    Created Floral class which extends the Services class

---
+++ src/main/java/nz/ac/auckland/se281/Floral.java
@@ -0,0 +1,15 @@
+package nz.ac.auckland.se281;
+
+public class Floral extends Services {
+
+  private final Types floralType;
+
+  public Floral(String bookingReference, Types floralType) {
+    super(bookingReference);
+    this.floralType = floralType;
+  }
+
+  public Types getFloralType() {
+    return this.floralType;
+  }
+}
