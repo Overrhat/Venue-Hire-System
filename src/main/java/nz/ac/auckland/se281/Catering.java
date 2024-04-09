89c2ac0f09c1bdc8b75f4e3fb5f1c6db5d0c7ded
Jeruh1105 <naruh061105@gmail.com>
2024-04-09 17:00:22 +1200

    Changed the variable name to start in lower case

--- src/main/java/nz/ac/auckland/se281/Catering.java
+++ src/main/java/nz/ac/auckland/se281/Catering.java
@@ -2,14 +2,14 @@ package nz.ac.auckland.se281;
 
 public class Catering extends Services {
 
-  private final Types CateringType;
+  private final Types cateringType;
 
-  public Catering(String bookingReference, Types CateringType) {
+  public Catering(String bookingReference, Types cateringType) {
     super(bookingReference);
-    this.CateringType = CateringType;
+    this.cateringType = cateringType;
   }
 
   public Types getCateringType() {
-    return this.CateringType;
+    return this.cateringType;
   }
 }
